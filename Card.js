cae5a1ad99b2d26cd5fc99f294d088350c4d4f93
Electrolytful <erhanulusal@hotmail.com>
2023-03-03 10:50:55 +0000

    Creation of 52 card deck array

--- Card.js
+++ Card.js
@@ -13,4 +13,24 @@ class Card {
     }
 }
 
-module.exports = Card;
+const suits = ["Hearts", "Clubs", "Spades", "Diamonds"];
+const ranks = ["A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2"];
+
+const cardsDeck = []
+
+for(let i = 0; i < 4; i++) {
+    for(let j = 0; j < 13; j++) {
+        let suit = suits[i];
+        let rank = ranks[j];
+
+        cardsDeck.push(new Card(rank, suit));
+    }
+}
+
+// console.log(cardsDeck);
+// console.log(cardsDeck.length);
+// console.log(cardsDeck[4].getValue());
+// console.log(cardsDeck[4].getSuit());
+
+module.exports = cardsDeck;
+
